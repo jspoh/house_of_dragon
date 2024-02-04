f8d741a582073a42410de93e911830abd5dd9e3c
jspoh <jingsengyeah@gmail.com>
2024-02-04 17:00:53 +0800

    wip - debugging texture ref

--- src/utils/Draw/Draw.h
+++ src/utils/Draw/Draw.h
@@ -17,6 +17,8 @@ Technology is prohibited.
 #pragma once
 
 #include "../utils.h"
+#include <unordered_map>
+#include <string>
 
 
 struct Color;
@@ -26,6 +28,8 @@ class Draw /*: Singleton<Draw>*/ {
 private:
 	static Draw* _instance;
 	AEGfxVertexList* _mesh;
+
+	std::unordered_map<std::string, AEGfxTexture*> _textureRef;
 
 	Draw();
 	~Draw();
@@ -49,4 +53,27 @@ public:
 	 * \param opacity [0.0, 1.0]
 	 */
 	void rect(f32 transX = 0, f32 transY = 0, Color color = Color{ 1,1,1,1 }, f32 scaleX = 50, f32 scaleY = 50, f32 rotation = 0, f32 opacity = 1);
+
+	/*texture stuff*/
+	/**
+	 * register a texture(img) for `.texture` method to work properly.
+	 * 
+	 * \param reference reference you want to use
+	 * \param path path to asset
+	 * \returns if registration was successful
+	 */
+	bool registerTexture(std::string reference, std::string path);
+
+	/**
+	 * get AEGfxTexture pointer by reference.
+	 * DO NOT free pointers returned by this function. call `removeTextureByRef` instead.
+	 * 
+	 * \param reference
+	 * \return 
+	 */
+	AEGfxTexture* getTextureByRef(std::string reference);
+
+	void removeTextureByRef(std::string reference);
+
+	void texture(std::string textureRef, f32 transX = 0, f32 transY = 0, Color color = Color{ 0,0,0,0 }, f32 scaleX = 50, f32 scaleY = 50, f32 rotation = 0, f32 opacity = 1);
 };
