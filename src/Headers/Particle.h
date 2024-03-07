c7944c4b3c2d43e4d15d90fccbea1a110ef83335
jspoh <jingsengyeah@gmail.com>
2024-03-07 10:45:21 +0800

    Revert "particle sys"
    This reverts commit 97c481046c8c36326fcb4d492e4d1d482cca5797.

--- src/Headers/Particle.h
+++ src/Headers/Particle.h
@@ -1,34 +1,48 @@
-#pragma once
-#ifndef PARTICLE
-#define PARTICLE
-
-#include "ParticleManager.h"
-
-
-
-class Particle 
-{
-public:
-	~Particle();
-
-	Particle(ParticleManager* _ParticleMgr);
-	static Particle* sInstance;
-
-
-	Point position;
-	Point velocity;
-	float size;
-	float lifetime;
-	float alpha;
-	bool active;
-	int type;
-
-
-
-
-private:
-	Particle();
-
-};
-
-#endif
+//#pragma once
+//#ifndef PARTICLE
+//#define PARTICLE
+//
+//
+//class ParticleManager;
+//class Particle : public GameObject
+//{
+//public:
+//	Particle();
+//	~Particle();
+//private:
+//	Particle(ParticleManager* _ParticleMgr); // This is used to register to ParticleManager
+//	static Particle* sInstance; // The pointer to the object that gets registered
+//
+//	const int maxParticles = 200;
+//
+//	// enum for different types of particle
+//	enum PARTICLE_TYPE {
+//		PLAYER_WALKING,
+//		ENEMY_DEATH
+//	};
+//	struct Point
+//	{
+//		float x, y;
+//	};
+//	// particle texture
+//
+//
+//	// particle object
+//	typedef struct {
+//		Point position;
+//		Point velocity;
+//		float size;
+//		float lifetime;
+//		float alpha;
+//		bool active;
+//		int type;
+//	}Particle;
+//	
+//	//Add your variables here
+//};
+//
+//void init_particle(Particle* particleArr, const char* fileName);
+//void emit_enemy_death_particle(Particle* particleArr, Point deadEnemyPos);
+//void update_particles(Particle* particleArr, float dt);
+//
+//#endif
