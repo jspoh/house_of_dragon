97c481046c8c36326fcb4d492e4d1d482cca5797
yuxjay205 <yangyujie205@gmail.com>
2024-03-07 10:33:35 +0800

    particle sys

--- src/Scenes/SceneCredit.cpp
+++ src/Scenes/SceneCredit.cpp
@@ -21,7 +21,7 @@ SceneCredits::SceneCredits()
 
 SceneCredits::SceneCredits(SceneManager* _sceneMgr)
 {
-	_sceneMgr->AddScene("SceneCredits", this);
+	_sceneMgr->AddScene("SceneCredit", this);
 }
 
 SceneCredits::~SceneCredits()
