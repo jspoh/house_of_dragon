01d08bb328f44d29aeb7da1694eb4933798ffbab
yuxjay205 <yangyujie205@gmail.com>
2024-04-03 12:24:55 +0800

    1

--- src/Scenes/HowToPlay.cpp
+++ src/Scenes/HowToPlay.cpp
@@ -1,3 +1,19 @@
+/* Start Header ************************************************************************/
+/*!
+\file HowToPlay.cpp
+\author Yang yujie, yujie.yang, 2301383
+\par yujie.yang\@digipen.edu
+\date 01 Apr 2024
+\brief  show how to play the game
+/*
+Copyright (C) 2024 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+
+
 #include "Pch.h"
 #include "HowToPlay.h"
 #include "SoundPlayer.h"
