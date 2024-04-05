d3a11e5bb9517dc9cb9c7a88db5415b7af97379d
yuxjay205 <yangyujie205@gmail.com>
2024-04-05 23:14:07 +0800

    minor changes

--- src/Headers/Pause.h
+++ src/Headers/Pause.h
@@ -24,16 +24,18 @@ private:
 
 	static constexpr u8 PAUSE_KEY = AEVK_ESCAPE;
 
-	static constexpr int NUM_BTNS = 3;
+	static constexpr int NUM_BTNS = 5;
 	std::array<std::pair<std::string, float>, NUM_BTNS> btns{
 		std::pair{"RESUME", 1.f},
+		{"HOWTOPLAY", 1.f},
 		{"RESTART", 1.f},
 		{"EXIT", 1.f}
+	
 	};
 
 	static constexpr int BTN_WIDTH = 250;
 	static constexpr int BTN_HEIGHT = 100;
-	static constexpr int BTN_Y_GAP = 50;
+	static constexpr int BTN_Y_GAP = 30;
 	static constexpr float HOVER_BTN_SCALE = 1.1f;
 
 public:
